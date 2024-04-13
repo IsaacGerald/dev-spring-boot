64ce2f1609df47f92c3adba8b258966cf6b41eff
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-04-13 10:37:25 +0300

    Added qualifier

--- src/main/java/com/geecode/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/geecode/springcoredemo/common/TrackCoach.java
@@ -3,9 +3,9 @@ package com.geecode.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach{
+public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes.";
+        return "Run a hard 5K!";
     }
 }
