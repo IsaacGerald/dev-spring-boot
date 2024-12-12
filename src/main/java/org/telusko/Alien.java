d38c0875a157662f5c4dfcdc1cb7cfd1a31e4d63
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-12 09:58:35 +0300

    setter and constructor injection

--- src/main/java/org/telusko/Alien.java
+++ src/main/java/org/telusko/Alien.java
@@ -1,9 +1,46 @@
 package org.telusko;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
+
+    private int age;
+
+    private int salary;
+    private Laptop lap;
+
+
+    public Alien(){
+        System.out.println("Object created");
+    }
+
+    @ConstructorProperties({"age", "lap"})
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter called");
+        this.age = age;
+    }
+
 
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 
 }
---
+++ src/main/java/org/telusko/Laptop.java
@@ -0,0 +1,12 @@
+package org.telusko;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop object create");
+    }
+
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
