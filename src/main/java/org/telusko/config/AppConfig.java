8adf248b8bd55769b3f257fffea1d5b48fc6c23a
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-13 07:51:35 +0300

    Java based: Qualifier and Primary

--- src/main/java/org/telusko/config/AppConfig.java
+++ src/main/java/org/telusko/config/AppConfig.java
@@ -1,19 +1,22 @@
 package org.telusko.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.telusko.Alien;
 import org.telusko.Computer;
 import org.telusko.Desktop;
+import org.telusko.Laptop;
 
 @Configuration
 public class AppConfig {
 
 
     @Bean
-    public Alien alien(@Autowired Computer com){
+    public Alien alien(Computer com){ //@Qualifier("desktop")
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setCom(com);
@@ -27,6 +30,12 @@ public class AppConfig {
     //@Scope("prototype")
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop(){
+        return new Laptop();
     }
 
 }
