de769fb63f4968a5a8361b0c569cc607124f9ca2
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-16 08:06:16 +0300

    Display data in result: Session way

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -2,6 +2,7 @@ package com.telusko.SpringBootWeb1;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,12 +16,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
         int num1  = Integer.parseInt(req.getParameter("num1"));
         int num2  = Integer.parseInt(req.getParameter("num2"));
 
         int result = num1 + num2;
-        System.out.println(result);
+       //Set result to session; gets hold of data between multiple web pages
+        session.setAttribute("result", result);
 
         return "result.jsp";
     }
