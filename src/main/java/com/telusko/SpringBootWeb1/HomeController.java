0c4652fd4b626ea42e62ac950870d60018ebbba9
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-17 07:23:27 +0300

    Model object

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,16 +14,17 @@ public class HomeController {
     @RequestMapping("/")
     public String  home(){
         System.out.println("Home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num_1, @RequestParam("num2") int num_2, HttpSession session){
+    public String add(@RequestParam("num1") int num_1, @RequestParam("num2") int num_2, Model model){
         int result = num_1 + num_2;
        //Set result to session; gets hold of data between multiple web pages
-        session.setAttribute("result", result);
+        //session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
-        return "result.jsp";
+        return "result";
     }
 
 }
