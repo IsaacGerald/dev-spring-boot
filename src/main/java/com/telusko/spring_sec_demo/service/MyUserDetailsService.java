7db4a13d80731a5de0223034919b53e39a6511ca
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 16:29:44 +0300

    User Details and User Principal

--- src/main/java/com/telusko/spring_sec_demo/service/MyUserDetailsService.java
+++ src/main/java/com/telusko/spring_sec_demo/service/MyUserDetailsService.java
@@ -2,6 +2,7 @@ package com.telusko.spring_sec_demo.service;
 
 import com.telusko.spring_sec_demo.dao.UserRepo;
 import com.telusko.spring_sec_demo.model.User;
+import com.telusko.spring_sec_demo.model.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,6 +27,6 @@ public class MyUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User 404");
         }
 
-        return null;
+        return new UserPrincipal(user);
     }
 }
