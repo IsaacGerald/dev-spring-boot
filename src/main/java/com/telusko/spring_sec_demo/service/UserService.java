b9100d239146a9f6b5f2c048893071f02fa87984
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 17:27:04 +0300

    BCrypt encoding for user registration

--- src/main/java/com/telusko/spring_sec_demo/service/UserService.java
+++ src/main/java/com/telusko/spring_sec_demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.telusko.spring_sec_demo.dao.UserRepo;
 import com.telusko.spring_sec_demo.model.Student;
 import com.telusko.spring_sec_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,12 @@ public class UserService {
 
     @Autowired
     UserRepo userRepo;
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public User saveUser(User user){
+
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
 
         return userRepo.save(user);
     }
