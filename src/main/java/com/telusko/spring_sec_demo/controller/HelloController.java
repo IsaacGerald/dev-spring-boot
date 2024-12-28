97ef4ac5eecb0af81cae1ac2cdc6cacc41a46146
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 15:57:26 +0300

    Creating UserDetailService

--- src/main/java/com/telusko/spring_sec_demo/controller/HelloController.java
+++ src/main/java/com/telusko/spring_sec_demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.telusko.spring_sec_demo;
+package com.telusko.spring_sec_demo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
