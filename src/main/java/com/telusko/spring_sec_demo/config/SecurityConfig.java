074b1f450788ccb087cd7cf9c3ade3180d4486c0
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 14:50:24 +0300

    without lambda

--- src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,17 +17,48 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //Disabling the csrf
-        http.csrf(AbstractHttpConfigurer::disable);
-        //Enable username and password
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        //Provide a form for username and password (can disable if session is stateless)
-        //http.formLogin(Customizer.withDefaults());
-        //User basic auth for security
-        http.httpBasic(Customizer.withDefaults());
-        //Make session stateless (Change after every request)
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        //With lambda
+
+//        //Disabling the csrf
+//        http.csrf(AbstractHttpConfigurer::disable);
+//        //Enable username and password
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        //Provide a form for username and password (can disable if session is stateless)
+//        //http.formLogin(Customizer.withDefaults());
+//        //User basic auth for security
+//        http.httpBasic(Customizer.withDefaults());
+//        //Make session stateless (Change after every request)
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        //Without lambda
+
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configure) {
+//             configure.disable();
+//            }
+//        };
+//
+//        http.csrf(custCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> custHttp =
+//                new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//                    @Override
+//                    public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+//                           authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+//                    }
+//                };
+//
+//        http.authorizeHttpRequests(custHttp);
+
+        //Another way with builder pattern
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
         return http.build();
