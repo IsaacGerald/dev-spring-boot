5676cd972b1b20d3f26cbd71078ad50f4921a585
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 16:09:57 +0300

    User Repository

--- src/main/java/com/telusko/spring_sec_demo/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.telusko.spring_sec_demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<> {
-}
--- src/main/java/com/telusko/spring_sec_demo/service/MyUserDetailsService.java
+++ src/main/java/com/telusko/spring_sec_demo/service/MyUserDetailsService.java
@@ -1,11 +1,14 @@
 package com.telusko.spring_sec_demo.service;
 
-import com.telusko.spring_sec_demo.repo.UserRepo;
+import com.telusko.spring_sec_demo.dao.UserRepo;
+import com.telusko.spring_sec_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -15,6 +18,14 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUsername(username);
+
+        if (user == null){
+            System.out.println("User 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+
         return null;
     }
 }
