ccbbd30fa0344cf46b930a0cf1b86a0f81de0d8e
Isaac Gerald <odhiambo.gerald@eclectics.io>
2024-12-28 14:29:57 +0300

    disabling csrf token

--- src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/com/telusko/spring_sec_demo/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.telusko.spring_sec_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +15,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //Disabling the csrf
+        http.csrf(AbstractHttpConfigurer::disable);
+        //Enable username and password
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        //Provide a form for username and password (can disable if session is stateless)
+        //http.formLogin(Customizer.withDefaults());
+        //User basic auth for security
+        http.httpBasic(Customizer.withDefaults());
+        //Make session stateless (Change after every request)
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 
 
         return http.build();
