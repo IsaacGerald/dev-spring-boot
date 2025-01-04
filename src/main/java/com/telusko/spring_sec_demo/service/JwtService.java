fee48728a58729b49811ceb86d17d32ea3b23bc6
Isaac Gerald <odhiambo.gerald@eclectics.io>
2025-01-04 12:30:40 +0300

    Setting AuthToken in SecurityContext

--- src/main/java/com/telusko/spring_sec_demo/service/JwtService.java
+++ src/main/java/com/telusko/spring_sec_demo/service/JwtService.java
@@ -54,4 +54,8 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String extractUserName(String token) {
+        return " ";
+    }
 }
